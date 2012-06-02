b81858ab496d6c6163b8c96ca1473b4b2b2408e1
Janosch Woschitz <janosch.woschitz@gmail.com>
2012-06-02 20:56:29 +0200

    minor changes (tank auth config)

--- application/config/tank_auth.php
+++ application/config/tank_auth.php
@@ -7,8 +7,8 @@
 | These details are used in emails sent by authentication library.
 |--------------------------------------------------------------------------
 */
-$config['website_name'] = 'Your project';
-$config['webmaster_email'] = 'webmaster@your-site.com';
+$config['website_name'] = 'Transfair';
+$config['webmaster_email'] = 'info@transfair.co';
 
 /*
 |--------------------------------------------------------------------------
@@ -48,7 +48,7 @@ $config['use_username'] = TRUE;
 
 $config['username_min_length'] = 4;
 $config['username_max_length'] = 20;
-$config['password_min_length'] = 4;
+$config['password_min_length'] = 8;
 $config['password_max_length'] = 20;
 
 /*
