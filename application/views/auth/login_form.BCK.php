fa9e568c07931395fad05b7f35f5313d83600feb
David Nagy <david@nagy.com>
2012-06-02 21:47:04 +0200

    Small copy changes. Captcha removed.

--- application/views/auth/login_form.BCK.php
+++ application/views/auth/login_form.BCK.php
@@ -43,6 +43,43 @@ $captcha = array(
 		<td><?php echo form_password($password); ?></td>
 		<td style="color: red;"><?php echo form_error($password['name']); ?><?php echo isset($errors[$password['name']])?$errors[$password['name']]:''; ?></td>
 	</tr>
+
+	<?php if ($show_captcha) {
+		if ($use_recaptcha) { ?>
+	<tr>
+		<td colspan="2">
+			<div id="recaptcha_image"></div>
+		</td>
+		<td>
+			<a href="javascript:Recaptcha.reload()">Get another CAPTCHA</a>
+			<div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type('audio')">Get an audio CAPTCHA</a></div>
+			<div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type('image')">Get an image CAPTCHA</a></div>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<div class="recaptcha_only_if_image">Enter the words above</div>
+			<div class="recaptcha_only_if_audio">Enter the numbers you hear</div>
+		</td>
+		<td><input type="text" id="recaptcha_response_field" name="recaptcha_response_field" /></td>
+		<td style="color: red;"><?php echo form_error('recaptcha_response_field'); ?></td>
+		<?php echo $recaptcha_html; ?>
+	</tr>
+	<?php } else { ?>
+	<tr>
+		<td colspan="3">
+			<p>Enter the code exactly as it appears:</p>
+			<?php echo $captcha_html; ?>
+		</td>
+	</tr>
+	<tr>
+		<td><?php echo form_label('Confirmation Code', $captcha['id']); ?></td>
+		<td><?php echo form_input($captcha); ?></td>
+		<td style="color: red;"><?php echo form_error($captcha['name']); ?></td>
+	</tr>
+	<?php }
+	} ?>
+
 	<tr>
 		<td colspan="3">
 			<?php echo form_checkbox($remember); ?>
@@ -52,5 +89,5 @@ $captcha = array(
 		</td>
 	</tr>
 </table>
-<?php echo form_submit('submit', 'Now... I login'); ?>
+<?php echo form_submit('submit', 'Let me in'); ?>
 <?php echo form_close(); ?>
