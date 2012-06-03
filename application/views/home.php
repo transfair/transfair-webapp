2ab97be6eb43ed985fee38a46bf28035388b095b
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2012-06-03 11:32:41 +0200

    Added home view with templates to view folder. Make use of these templates whenever possible. Copy template folder into sub view directories to adapt certain parts (e.g. the sidebar.php when you're logged in)

---
+++ application/views/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<?php include "./templates/head.php"; ?>
+<body>
+<div class="container">
+	<?php include "./templates/header.php"; ?>
+	<div class="row">
+		<div class="span8">
+			<?php include "./templates/nav-tabs.php"; ?>
+				
+				<!-- CONTENT GOES HERE -->
+				
+		</div> <!-- /.span8 -->
+		<?php include "./templates/sidebar.php"; ?> 
+	</div> <!-- /.row -->
+</div> <!-- /.container -->
+<?php include "./templates/foot.php"; ?>
+</body></html>
