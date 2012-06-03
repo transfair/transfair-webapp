7dd2d1b4c8c5e40252cc8a8ae83628a95a9aaa4c
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2012-06-03 14:25:55 +0200

    Minor fix, removed bracket

--- application/views/templates/nav-tabs.php
+++ application/views/templates/nav-tabs.php
@@ -1,6 +1,6 @@
 	<nav>
 		<ul class="nav nav-tabs">
-			<li><?php echo anchor('/', 'About Transfair') ?><</li>
+			<li><?php echo anchor('/', 'About Transfair') ?></li>
             <li><?php echo anchor('/client/about', 'Create a job') ?></li>
 			<li><?php echo anchor('/serviceProvider/register', 'Become a partner') ?></li>
 		</ul>
