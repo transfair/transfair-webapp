3a17ad38f694e5102589e86278de6416c11a914b
David Owens <david@fineartdavid.com>
2012-06-03 15:32:11 +0200

    remove temporary templates

--- head.php
+++
@@ -1,7 +0,0 @@
-<head>
-	<meta charset="utf-8">
-	<title>Transfair</title>
-	<meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/styles.css">
-</head>
