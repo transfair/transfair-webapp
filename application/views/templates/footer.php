361b060decd1e5032b5aa1f5fbc345996fae6c7a
David Owens <david@fineartdavid.com>
2012-06-03 16:01:13 +0200

    footer layout changes

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -37,11 +37,14 @@
 			</ul>
 		</div>
 	</div>
-
-	<div class="connect"
-		<a href="http://twitter.com/transfair"> <img src="http://transfair.co/wp-content/themes/nautilius/images/twitter.png" alt="twitter"> </a> 
-		<a href="http://www.facebook.com/pages/transfair/163236270399309"> <img src=" http://transfair.co/wp-content/themes/nautilius/images/facebook.png" alt="facebook"> </a> 
-		<a href="http://transfair.co/rss"> <img src=" http://transfair.co/wp-content/themes/nautilius/images/rss.png" alt="rss"> </a>
+	<div class="row">
+		<div class="span9">
+			<div class="connect"
+				<a href="http://twitter.com/transfair"> <img src="http://transfair.co/wp-content/themes/nautilius/images/twitter.png" alt="twitter"> </a> 
+				<a href="http://www.facebook.com/pages/transfair/163236270399309"> <img src=" http://transfair.co/wp-content/themes/nautilius/images/facebook.png" alt="facebook"> </a> 
+				<a href="http://transfair.co/rss"> <img src=" http://transfair.co/wp-content/themes/nautilius/images/rss.png" alt="rss"> </a>
+			</div>
+			<div class="copyinfo">© 2012 transfair ltd.</div>
+		</div>
 	</div>
-	<div class="copyinfo">© 2012 transfair ltd.</div>
 </div>
