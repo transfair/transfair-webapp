1a000550b70be43ea91edbb9c29773ea98e6a0cb
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2012-06-03 14:50:24 +0200

    Fixed the welcome page bugs

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -18,7 +18,7 @@ class Welcome extends CI_Controller
             'hero' => $this->load->view('templates/hero', array(), TRUE),
             'navtabs' => $this->load->view('templates/nav-tabs', array(), TRUE),
             'sidebar' => $this->load->view('templates/sidebar', array(), TRUE),
-            'footer' => $this->load->view('templates/footer', array(), TRUE),
+            'footer' => $this->load->view('templates/big-footer', array(), TRUE),
             'foot' => $this->load->view('templates/foot', array(), TRUE)
         );
 
