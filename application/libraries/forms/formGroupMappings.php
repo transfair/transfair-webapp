28a8e624e6fd9d148aecf010983fac6953b8c4e4
Ellen König <ellen@ellenkoenig.de>
2012-06-03 14:03:44 +0200

    FormGroupMappings.php

--- application/libraries/forms/formGroupMappings.php
+++ application/libraries/forms/formGroupMappings.php
@@ -1,20 +1,34 @@
 <?php
     $ServiceProviderGroups = array(
-        "Personal Information" => array(
-            "FirstName" => "Please enter your first name", 
-            "LastName" => "Please enter your last name", 
-            "Gender" => ""
+        "Personal information" => array(
+            "FirstName" => "your given name", 
+            "LastName" => "your family name", 
+            "Gender" => "",
+			"YearOfBirth" => "Please enter your date of birth in this format dd.MM.yyyy",
+			"Emailadress" => "your given name", 
+            "Mobile" => "You don't need a mobile phone, but if you have one we can inform you by text message (sms)."
         ), 
-        "Address" => array(
-            "MailingStreet" => "What street do you live in?", 
-            "MailingCity" => "", 
-            "MailingZip" => "", 
-            "MailingCountry" => ""
+		
+		"Where do you live" => array(
+            "Country" => "What street do you live in?", 
+            "NameOfCity/Village" => "", 
+            "Ruural/Urban" => ""
         ),
-        "Test" => array(
-            "Boo" => "Empty"
+
+		"Educational background" => array(
+            "HighestLevelOfEducationCompleted" => "", 
+            "Education" => "Please tell us a little bit about your educational background. What did you learn? Where and how long?", 
+            "WorkExperience" => "If you have worked before, please tell us a little bit about your previous work experience. What have you been doing and what are you doing now?",
+			"NativeLanguage" => "Which language do you speak best?", 
+            "English" => "",
+			"French" => "",
+			"German" => "", 
+			"Spanish" => "", 
+            "Skills" => "Which service would you like to offer?",
+	        "AdditionalLanguageSkills" => ""
         )
-    );
+		
+	);
 
     $ClientGroups = array(
         "Personal Information" => array(
